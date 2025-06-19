5d698e31fdd7ca6d4e921bb2b97f8ef44d973115
zhangcongcong <zcc1012tute@sina.com>
2025-06-19 22:58:55 +0800

    创建项目

---
+++ src/main/java/com/leinao/leinaoaiagent/LeinaoAiAgentApplication.java
@@ -0,0 +1,13 @@
+package com.leinao.leinaoaiagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeinaoAiAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeinaoAiAgentApplication.class, args);
+    }
+
+}
